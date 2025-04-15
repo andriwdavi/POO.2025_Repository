0853cfa428dd950faad3455f667895d4199eebdd
Andriw <andriw30davi@gmail.com>
2025-04-15 13:31:37 +0000

    oi

--- POO_Class/Lesson_02/exercises.py
+++ POO_Class/Lesson_02/exercises.py
@@ -1,1 +1,18 @@
 
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------#
+#import math
+#height = int(input())
+#print(f"Area = {(base * height):.2f} Perimeter = {((base + height) * 2):.2f} Diagonal = {(math.sqrt(base ** 2 + height ** 2)):.2f}")
+#---------------------------------------------------------------------------------------------------------------------------------#
+#grade1 = int(input("Type your first bimester grade\n"))
+#grade2 = int(input("Type your second bimester grade\n"))
+#finalGrade = ((grade1 * 2) + (grade2 * 3)) / 5
+#print(f"Your final grade was {finalGrade}")
