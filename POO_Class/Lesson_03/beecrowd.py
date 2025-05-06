7fd5f5feb8659ef36af39459b2d1edac78b4fd97
Andriw <andriw30davi@gmail.com>
2025-05-06 12:20:06 +0000

    .

--- POO_Class/Lesson_03/beecrowd.py
+++ POO_Class/Lesson_03/beecrowd.py
@@ -1,1 +1,25 @@
 
+"""
+CandN = input()
+splitedCandN = CandN.split()
+intSplitedCandN = [int(item) for item in splitedCandN]
+C = int(splitedCandN[0])
+N = int(splitedCandN[1])
+END = C % N
+print(END)
+
+x = int(input())
+y = int(input())
+PROD = x * y
+print(f"PROD = {PROD}")
+
+x = float(input())
+y = float(input())
+MEDIA = ((x * 3.5) + (y * 7.5)) / 11
+print(f"MEDIA = {MEDIA:.5f}")
+
+RAIO = float(input())
+pi = 3.14159
+VOLUME = (4.0 / 3) * pi * RAIO ** 3
+print(f"VOLUME = {VOLUME:.3f}")
+"""
